d65192a7587b8d25b9282003b9cd7c464146e1cc
myk245 <myk245@nyu.edu>
2020-05-28 14:40:33 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
-// add code snippets from README
+let state;
+
+// set initial state in reducer by using a default argument
+function reducer(state = { count: 0 }, action) {
+   switch (action.type) {
+      case 'INCREASE_COUNT':
+         return { count: state.count + 1 }
+      default:
+         return state;
+   }
+}
+
+function dispatch(action) {
+   state = reducer(state, action)
+   render()
+}
+
+function render() {
+   let container = document.getElementById('container');
+   container.textContent = state.count;
+}
+
+dispatch({ type: '@@INIT' })
+
+// event listener that integrates dispatch with a user event
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+   dispatch({ type: 'INCREASE_COUNT' })
+})
+
